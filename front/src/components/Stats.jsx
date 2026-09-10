63b080002df6c0e2ff93f2e38631296ff269705d
MaxCho <makibaypro@gmail.com>
2026-09-10 11:17:39 +0200

    Ajout du stats.jsx pour l'affichage des donnee

--- front/src/components/Stats.jsx
+++ front/src/components/Stats.jsx
@@ -1,24 +1,51 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, Fragment } from "react";
+import "./Stats.css"
 
 const Stats = () => {
 
-    const []=useStats();
+    const API = 'http://localhost:3000/api';
+
+    const [objetStatut, setObjetStatut] = useState([]);
+    const [poidsTotal, setPoidsTotal] = useState(-1);
+    const [nbObjetRayon, setNbObjetRayon] = useState(-1);
 
     const chargerDonnees = async () => {
 
         const data = await fetch (`${API}/stats`);
-        const dataJson = data.json();
+        const dataJson = await data.json();
 
-        
+        setObjetStatut(dataJson.objetsStatut);
+        setPoidsTotal(dataJson.poidsTotal);
+        setNbObjetRayon(dataJson.nbObjetRayon);    
     }
 
-
-
-
+    useEffect(() => {
+        chargerDonnees();
+    }, []);
 
     return (
-        <>
+        <section>
+            <div className="enRayon">
+                <p>Objets en rayon :</p>
+                <p>{nbObjetRayon}</p>
+            </div>
+            <div className="poidsTotal">
+                <p>Poids Total :</p>
+                <p>{poidsTotal}</p>
+            </div>
+            <div>
+                <dl className="stats-donneeTableau">
+                    {objetStatut.map((element, index) => (
+                        <Fragment key={index}>
+                            <dt className="donneeTitle">{element.statut}</dt>
+                            <dd className="donneeDetail">{element.nombre}</dd>
+                        </Fragment>
+                    ))}
+                </dl>
+            </div>
 
-
-        </>
+        </section>
     )}
+
+
+    export default Stats;
