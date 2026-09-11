8b792aa24a48071bc98dfa080dca232c40457328
Marin Nicolle Poussier <147306461+L6EAU9BIO@users.noreply.github.com>
2026-09-11 23:39:16 +0200

    Fonctionnalité de l'objet ouvrant opérationelle + Modif CSS Objetslite et Filtres

--- front/src/components/Objetsliste.jsx
+++ front/src/components/Objetsliste.jsx
@@ -9,6 +9,9 @@ function Objetsliste () {
     const [categories , setCategories] = useState([]);
     const [categorieFiltre, setCategorieFiltre] = useState("")
 
+    const [selectObjet, setSelectObjet] = useState("")
+    const [selectObjetContent, setSelectObjetContent] = useState("")
+
     const handleStatutChange = (nouveauStatut) => {
         setStatutFiltre(nouveauStatut);
     }
@@ -16,6 +19,14 @@ function Objetsliste () {
     const handleCategorieChange = (nouvelleCategorie) => {
         setCategorieFiltre(nouvelleCategorie);
     }
+
+    const handleObjetChange = (nouveauObjet) => {
+        if (nouveauObjet === selectObjet) {
+            return setSelectObjet("")
+        }
+        setSelectObjet(nouveauObjet);
+    }
+
     
     useEffect(() => {
         async function chargerObjets() {
@@ -48,6 +59,18 @@ function Objetsliste () {
         } 
         chargerCategories();
     }, []);
+
+    useEffect(() => {
+        async function chargerSelectObjet() {
+            if (!selectObjet) {
+                return 
+            }
+            const reponse = await fetch(`http://localhost:3000/api/objets/${selectObjet}`)
+            const data = await reponse.json(); 
+            setSelectObjetContent(data)
+        }
+        chargerSelectObjet();
+    }, [selectObjet])
     return (
         <>
         <div className="filtre_objets_box">
@@ -55,13 +78,18 @@ function Objetsliste () {
             <Filtres statut={statutFiltre} onStatutChange={handleStatutChange} categories={categories} categorieFiltre={categorieFiltre} onCategorieChange={handleCategorieChange}/>
             <ul>
                 {objets.map((objet) =>(
-                    <li key={objet.id} className="objets">{objet.libelle} - Catégorie : {objet.categorie} - Prix : {objet.prix} € - Statut de l'objet : {objet.statut} </li>
-
+                    <li 
+                    key={objet.id} 
+                    className="objetsliste-objet" 
+                    onClick={() => handleObjetChange(objet.id)}>
+                        {objet.libelle} --- Catégorie : {objet.categorie} --- Prix : {objet.prix} € --- Statut de l'objet : {objet.statut} 
+                        {selectObjet === objet.id && (` --- Etat : ${selectObjetContent.etat_arrivee} --- Date de mise en rayon : ${new Date(selectObjetContent.date_mise_rayon).toLocaleDateString('fr-FR')}`)} </li>
+                    
+                    
                 ))}
             </ul>
         
         </div> 
-        
         </>
     )
 };
