0cd9f0c039cc31cf25c08a89b4cc86dc7b9c0124
Marin Nicolle Poussier <147306461+L6EAU9BIO@users.noreply.github.com>
2026-09-11 12:26:02 +0200

    Création du composnat Filtres.jsx ainsi que la route lui permettant son existence et fonctionnement, requetes http, css, js

--- front/src/components/Filtres.jsx
+++ front/src/components/Filtres.jsx
@@ -1,6 +1,34 @@
 import { useState, useEffect } from "react";
 import './Filtres.css'
 
-function Filtres () {
-    const [filtres, setFiltres] = useState([])
-}
+export default function Filtres ({ statut, onStatutChange, categories, categorieFiltre, onCategorieChange }) {
+    return (
+        <>
+        <div className="filtres_box">
+            
+            <h2>Filtres</h2>
+            <p>Par statut :</p>
+            <select value={statut} onChange={(e) => onStatutChange(e.target.value)}>
+                <option value="">Tous statuts</option>
+                <option value="arrive">Arrivé</option>
+                <option value="en_reparation">En réparation</option>
+                <option value="en_rayon">En rayon</option>
+                <option value="vendu">Vendu</option>
+                <option value="recycle">Recycle</option>
+            </select>
+            
+            <p>Par catégorie</p>
+
+            <select value={categorieFiltre} onChange={(e) => onCategorieChange(e.target.value)}>
+                <option value="">Toutes catégories</option>
+                {categories.map((categorie) => (
+                    <option key={categorie.id} className="categories" value={categorie.id}>{categorie.libelle}</option>
+                ))}
+            </select>
+        
+        </div>    
+        </>
+    )
+};
+
+
