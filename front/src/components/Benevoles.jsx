1ef3595e5a7368f3d56b59b4105b40188f47a998
MaxCho <makibaypro@gmail.com>
2026-09-11 16:50:41 +0200

    Ajout des gestion d'erreur en front pour le composant benevoles

--- front/src/components/Benevoles.jsx
+++ front/src/components/Benevoles.jsx
@@ -6,13 +6,22 @@ const Benevoles = ({onSelect}) => {
     const API = 'http://localhost:3000/api';
 
     const [listeBenevoles, setListeBenevoles] = useState([]);
+    const [erreur, setErreur] = useState(null);
 
     const chargerListe = async () => {
+        try {
+            const response = await fetch(`${API}/benevoles`);
 
-        const data = await fetch(`${API}/benevoles`);
-        const dataJson = await data.json();
+            if(!response.ok){
+                throw new Error(`Erreur ${response.status}: Impossible de charger la liste des bénévoles`);
+            }
 
-        setListeBenevoles(dataJson);
+            const data = await response.json();
+            setListeBenevoles(data);
+    } catch (error) {
+        console.error(error.message);
+        setErreur(error.message)
+    }
 
     }
 
@@ -20,12 +29,20 @@ const Benevoles = ({onSelect}) => {
         chargerListe()
     }, [])
 
+    if(erreur){
+        return(
+            <>
+            <h3>Oops !</h3>
+            <p>{erreur}</p>
+            </>
+        )};
+
     return (
         <>
         <h2 className="benevoles-title">Identifiez-vous :</h2>
         <section className="benevoles-boutonDisplay">
-            {listeBenevoles.map((element, index) => 
-                <button key={index} className="benevoles-boutonBenevole" type="button" onClick={() => onSelect(element.nom, element.prenom)}>{element.nom} {element.prenom}</button>
+            {listeBenevoles.map((element) => 
+                <button key={element.id} className="benevoles-boutonBenevole" type="button" onClick={() => onSelect(element.nom, element.prenom)}>{element.nom} {element.prenom}</button>
             )}
         </section>
         </>
