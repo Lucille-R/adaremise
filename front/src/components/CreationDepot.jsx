a36ec133ac68b30c80cd85afbdf4f6dccc1c7e95
Marin Nicolle Poussier <147306461+L6EAU9BIO@users.noreply.github.com>
2026-09-13 14:59:56 +0200

    Ajout d'un CSS et class si necessire à CréationDepot

--- front/src/components/CreationDepot.jsx
+++ front/src/components/CreationDepot.jsx
@@ -100,7 +100,7 @@ const CreationDepot = () => {
 
 	return (
 		<>
-			<h2>Nouveau dépôt :</h2>
+			<h2 className="creationDepot-titre">Nouveau dépôt :</h2>
 
 			<form onSubmit={handleSubmit}>
 				
