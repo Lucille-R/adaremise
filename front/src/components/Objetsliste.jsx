2fedb6e327fcbdb9b9b0ce73f184866225fa6d8c
Marin Nicolle Poussier <147306461+L6EAU9BIO@users.noreply.github.com>
2026-09-13 12:06:54 +0200

    Création de la foncftionnalité boutton valider statut

--- front/src/components/Objetsliste.jsx
+++ front/src/components/Objetsliste.jsx
@@ -5,12 +5,17 @@ import Filtres from './Filtres.jsx'
 
 function Objetsliste () {
     const [objets, setObjets] = useState([]);
+    
     const [statutFiltre, setStatutFiltre] = useState("");
+    
     const [categories , setCategories] = useState([]);
     const [categorieFiltre, setCategorieFiltre] = useState("")
 
     const [selectObjet, setSelectObjet] = useState("")
     const [selectObjetContent, setSelectObjetContent] = useState("")
+
+    const [curentSelectStatut, setCurentSelectStatut] = useState("")
+
 
     const handleStatutChange = (nouveauStatut) => {
         setStatutFiltre(nouveauStatut);
@@ -27,27 +32,41 @@ function Objetsliste () {
         setSelectObjet(nouveauObjet);
     }
 
+    const handleModifStatut = (modifStatut) => {
+        setCurentSelectStatut(modifStatut);
+    }
+
+    const handleValidStatut = async (idObjet) => {
+        await fetch(`http://localhost:3000/api/objets/${idObjet}/statut` , {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({statut : curentSelectStatut})
+            });
+
+            chargerObjets();
+    }
+
+    async function chargerObjets() {
+        const params = new URLSearchParams();
+
+        if (statutFiltre) {
+            params.append('statut', statutFiltre);
+        }
+        if (categorieFiltre) {
+            params.append('categorie_id', categorieFiltre);
+        }
+
+        const queryString = params.toString();
+        const url = queryString
+            ? `http://localhost:3000/api/objets?${queryString}`
+            : 'http://localhost:3000/api/objets';
+        
+            const reponse = await fetch(url);
+        const data = await reponse.json();
+        setObjets(data);
+    }
     
     useEffect(() => {
-        async function chargerObjets() {
-            const params = new URLSearchParams();
-
-            if (statutFiltre) {
-                params.append('statut', statutFiltre);
-            }
-            if (categorieFiltre) {
-                params.append('categorie_id', categorieFiltre);
-            }
-
-            const queryString = params.toString();
-            const url = queryString
-                ? `http://localhost:3000/api/objets?${queryString}`
-                : 'http://localhost:3000/api/objets';
-            
-                const reponse = await fetch(url);
-            const data = await reponse.json();
-            setObjets(data);
-        }
         chargerObjets();
     }, [statutFiltre, categorieFiltre]);
     
@@ -76,7 +95,6 @@ function Objetsliste () {
         <div className="filtre_objets_box">
         
             <Filtres statut={statutFiltre} onStatutChange={handleStatutChange} categories={categories} categorieFiltre={categorieFiltre} onCategorieChange={handleCategorieChange}/>
-            <ul>
                 <h2 className="objetsListeTitre">Stock de La Remise</h2>
 
                 <section className="objetsListeOnboarding">
@@ -84,17 +102,43 @@ function Objetsliste () {
                             Vous pouvez filtrer chaque resultat grâce à la barre située à votre gauche, ainsi qu'obtenir plus d'informations sur un objets particulier en cliquant sur son bandeau.
                         </p>
                 </section>
+                
+        <div className="objetliste-bandeau">
+
+            <ul>
                 {objets.map((objet) =>(
                     <li 
                     key={objet.id} 
                     className="objetsliste-objet" 
                     onClick={() => handleObjetChange(objet.id)}>
                         {objet.libelle} --- Catégorie : {objet.categorie} --- Prix : {objet.prix} € --- Statut de l'objet : {objet.statut} 
-                        {selectObjet === objet.id && (` --- Poids : ${selectObjetContent.poids_kg} kg --- Etat : ${selectObjetContent.etat_arrivee} --- Date de mise en rayon : ${selectObjetContent.date_mise_rayon ? new Date(selectObjetContent.date_mise_rayon).toLocaleDateString('fr-FR') : "Aucune date renseignée"}`)} </li>
-                    
-                    
+                        {selectObjet === objet.id && (
+                            <div onClick={(e) => e.stopPropagation()}>
+                                <p>
+
+                                {` --- Poids : ${selectObjetContent.poids_kg} kg --- Etat : ${selectObjetContent.etat_arrivee} --- Date de mise en rayon : ${selectObjetContent.date_mise_rayon ? new Date(selectObjetContent.date_mise_rayon).toLocaleDateString('fr-FR') : "Aucune date renseignée"}`}
+
+                                </p>
+
+                                <select value={curentSelectStatut} onChange={(e) => setCurentSelectStatut(e.target.value)}className="objetliste-selectstatut">
+                                    <option value="arrive">Arrivé</option>
+                                    <option value="en_reparation">En reparation</option>
+                                    <option value="en_rayon">En rayon</option>
+                                    <option value="vendu">Vendu</option>
+                                    <option value="recycle">Recycle</option>
+                                </select>
+                
+                                <button className="objetlist-valid" onClick={() => handleValidStatut(objet.id)}>Modifier</button>
+                                
+                            </div>
+                        )}
+                    </li>
+    
                 ))}
+                
             </ul>
+        
+        </div>
         
         </div> 
         </>
